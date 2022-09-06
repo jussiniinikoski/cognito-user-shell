d035e19ef13f0c8f32a4c54a1becf476af7fa46a
jussiniinikoski <jussi.niinikoski@perjantai.fi>
2022-09-06 12:31:19 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,19 +5,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cognito-user-shell",  # Replace with your own username
-    version="0.1.5",
+    version="0.1.6",
     author="Jussi Niinikoski",
     author_email="jussi.niinikoski@perjantai.fi",
     description="Extendable shell/CLI tool with AWS Cognito authentication.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jussiniinikoski/cognito-user-shell",
-    packages=['cognito_user_shell', ],
-    install_requires=['boto3', 'requests', ],
+    packages=[
+        "cognito_user_shell",
+    ],
+    install_requires=[
+        "boto3",
+        "requests",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
