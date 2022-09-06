4d5518115273b42dfea48be1e060e67c318cc831
jussiniinikoski <jussi.niinikoski@perjantai.fi>
2022-09-06 12:24:39 +0300

    software token challenge

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Example package shows how you can expand and implement your own tools.
 git clone https://github.com/jussiniinikoski/cognito-user-shell.git
 cd cognito-user-shell
 # Set up some environment variables
+# set PYTHONPATH to current directory if didn't pip install
+export PYTHONPATH=./
 export COGNITO_CLIENT_ID=add-your-aws-cognito-client-id-here
 export API_URL=http://127.0.0.1:8000
 python example
--- cognito_user_shell/client.py
+++ cognito_user_shell/client.py
@@ -29,7 +29,9 @@ class UserClient:
         self.id_token = None
         self.expires = datetime.now()
         # Anonymous client connection without credentials
-        self.aws_client = boto3.client('cognito-idp', config=Config(signature_version=UNSIGNED))
+        self.aws_client = boto3.client(
+            "cognito-idp", config=Config(signature_version=UNSIGNED)
+        )
         self.cognito_client_id = cognito_client_id
         self.api_url = api_url
 
@@ -56,11 +58,8 @@ class UserClient:
         try:
             response = self.aws_client.initiate_auth(
                 ClientId=self.cognito_client_id,
-                AuthFlow='USER_PASSWORD_AUTH',
-                AuthParameters={
-                    'USERNAME': username,
-                    'PASSWORD': password
-                }
+                AuthFlow="USER_PASSWORD_AUTH",
+                AuthParameters={"USERNAME": username, "PASSWORD": password},
             )
         except self.aws_client.exceptions.UserNotFoundException:
             raise AuthUserNotFoundException
@@ -68,17 +67,19 @@ class UserClient:
             raise AuthIncorrectUsernameOrPasswordException
         except self.aws_client.exceptions.InvalidParameterException:
             raise AuthIncorrectUsernameOrPasswordException
-        authentication_result = response.get('AuthenticationResult', None)
+        authentication_result = response.get("AuthenticationResult", None)
         if authentication_result is not None:
             access_token = authentication_result.get("AccessToken", None)
             refresh_token = authentication_result.get("RefreshToken", None)
             id_token = authentication_result.get("IdToken", None)
-            self._update_tokens(access_token=access_token,
-                                refresh_token=refresh_token,
-                                id_token=id_token)
+            self._update_tokens(
+                access_token=access_token,
+                refresh_token=refresh_token,
+                id_token=id_token,
+            )
             expires = authentication_result.get("ExpiresIn", 3600)
             self._update_expiration(expires)
-        elif response.get("ChallengeName", None) == 'NEW_PASSWORD_REQUIRED':
+        elif response.get("ChallengeName", None) == "NEW_PASSWORD_REQUIRED":
             session = response.get("Session", None)
             print("New password required.\n")
             new_password = getpass.getpass("Enter new password: ")
@@ -89,79 +90,114 @@ class UserClient:
             try:
                 response = self.aws_client.respond_to_auth_challenge(
                     ClientId=self.cognito_client_id,
-                    ChallengeName='NEW_PASSWORD_REQUIRED',
+                    ChallengeName="NEW_PASSWORD_REQUIRED",
                     Session=session,
                     ChallengeResponses={
-                        'USERNAME': username,
-                        'NEW_PASSWORD': new_password,
-                    }
+                        "USERNAME": username,
+                        "NEW_PASSWORD": new_password,
+                    },
                 )
             except self.aws_client.exceptions.UserNotFoundException:
                 raise AuthUserNotFoundException
             except self.aws_client.exceptions.NotAuthorizedException:
                 raise AuthIncorrectUsernameOrPasswordException
-            except (self.aws_client.exceptions.InvalidPasswordException,
-                    self.aws_client.exceptions.InvalidParameterException):
+            except (
+                self.aws_client.exceptions.InvalidPasswordException,
+                self.aws_client.exceptions.InvalidParameterException,
+            ):
                 raise AuthInvalidPasswordException
-            authentication_result = response.get('AuthenticationResult', None)
-            if authentication_result is not None:
-                access_token = authentication_result.get("AccessToken", None)
-                refresh_token = authentication_result.get("RefreshToken", None)
-                id_token = authentication_result.get("IdToken", None)
-                self._update_tokens(access_token=access_token,
-                                    refresh_token=refresh_token,
-                                    id_token=id_token)
-                expires = authentication_result.get("ExpiresIn", 3600)
-                self._update_expiration(expires)
+            authentication_result = response.get("AuthenticationResult", None)
+        elif response.get("ChallengeName", None) == "SOFTWARE_TOKEN_MFA":
+            session = response.get("Session", None)
+            otp = getpass.getpass("Enter one-time password: ")
+            try:
+                response = self.aws_client.respond_to_auth_challenge(
+                    ClientId=self.cognito_client_id,
+                    ChallengeName="SOFTWARE_TOKEN_MFA",
+                    Session=session,
+                    ChallengeResponses={
+                        "USERNAME": username,
+                        "SOFTWARE_TOKEN_MFA_CODE": otp,
+                    },
+                )
+            except self.aws_client.exceptions.UserNotFoundException:
+                raise AuthUserNotFoundException
+            except self.aws_client.exceptions.NotAuthorizedException:
+                raise AuthIncorrectUsernameOrPasswordException
+            except (
+                self.aws_client.exceptions.InvalidPasswordException,
+                self.aws_client.exceptions.InvalidParameterException,
+            ):
+                raise AuthInvalidPasswordException
+            authentication_result = response.get("AuthenticationResult", None)
+        if authentication_result is not None:
+            access_token = authentication_result.get("AccessToken", None)
+            refresh_token = authentication_result.get("RefreshToken", None)
+            id_token = authentication_result.get("IdToken", None)
+            self._update_tokens(
+                access_token=access_token,
+                refresh_token=refresh_token,
+                id_token=id_token,
+            )
+            expires = authentication_result.get("ExpiresIn", 3600)
+            self._update_expiration(expires)
 
     def refresh_tokens(self):
         """Refresh tokens."""
         response = self.aws_client.initiate_auth(
             ClientId=self.cognito_client_id,
-            AuthFlow='REFRESH_TOKEN_AUTH',
-            AuthParameters={
-                'REFRESH_TOKEN': self.refresh_token
-            }
+            AuthFlow="REFRESH_TOKEN_AUTH",
+            AuthParameters={"REFRESH_TOKEN": self.refresh_token},
         )
         print("Refresh token used.")
-        authentication_result = response.get('AuthenticationResult', None)
+        authentication_result = response.get("AuthenticationResult", None)
         if authentication_result is not None:
             access_token = authentication_result.get("AccessToken", None)
             refresh_token = authentication_result.get("RefreshToken", None)
             id_token = authentication_result.get("IdToken", None)
-            self._update_tokens(access_token=access_token,
-                                refresh_token=refresh_token,
-                                id_token=id_token)
+            self._update_tokens(
+                access_token=access_token,
+                refresh_token=refresh_token,
+                id_token=id_token,
+            )
             expires = authentication_result.get("ExpiresIn", 3600)
             self._update_expiration(expires)
 
-    def _call(self, method='GET', endpoint=None, data=None, json=None, callback=None, callback_kwargs=None):
+    def _call(
+        self,
+        method="GET",
+        endpoint=None,
+        data=None,
+        json=None,
+        callback=None,
+        callback_kwargs=None,
+    ):
         """Unified method for calling REST APIs, contains Authorization header with Cognito tokens."""
         if endpoint is None:
             return
         self.check_auth()
-        headers = {'Authorization': f'Bearer {self.id_token}'}
-        api_kwargs = {'headers': headers}
+        headers = {"Authorization": f"Bearer {self.id_token}"}
+        api_kwargs = {"headers": headers}
         if method == "GET":
             api_call = requests.get
-            api_kwargs.update({'params': data})
+            api_kwargs.update({"params": data})
         elif method == "POST":
             api_call = requests.post
-            api_kwargs.update({'data': data, 'json': json})
+            api_kwargs.update({"data": data, "json": json})
         elif method == "PUT":
             api_call = requests.put
-            api_kwargs.update({'data': data, 'json': json})
+            api_kwargs.update({"data": data, "json": json})
         elif method == "PATCH":
             api_call = requests.patch
-            api_kwargs.update({'data': data, 'json': json})
+            api_kwargs.update({"data": data, "json": json})
         elif method == "DELETE":
             api_call = requests.delete
-            api_kwargs.update({'data': data, 'json': json})
+            api_kwargs.update({"data": data, "json": json})
         else:
             print("Illegal API method.")
             return
         try:
-            r = api_call(f'{self.api_url}/{endpoint}', **api_kwargs)
+            r = api_call(f"{self.api_url}/{endpoint}", **api_kwargs)
         except requests.exceptions.ConnectionError:
             print("Connection Error!")
             return
@@ -178,44 +214,62 @@ class UserClient:
 
     def get(self, endpoint=None, data=None, callback=None, callback_kwargs=None):
         """Call api (GET) and send response to callback."""
-        return self._call("GET",
-                          endpoint=endpoint,
-                          data=data,
-                          callback=callback,
-                          callback_kwargs=callback_kwargs)
+        return self._call(
+            "GET",
+            endpoint=endpoint,
+            data=data,
+            callback=callback,
+            callback_kwargs=callback_kwargs,
+        )
 
-    def post(self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None):
+    def post(
+        self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None
+    ):
         """Call api (POST) and send response to callback."""
-        return self._call("POST",
-                          endpoint=endpoint,
-                          data=data,
-                          json=json,
-                          callback=callback,
-                          callback_kwargs=callback_kwargs)
+        return self._call(
+            "POST",
+            endpoint=endpoint,
+            data=data,
+            json=json,
+            callback=callback,
+            callback_kwargs=callback_kwargs,
+        )
 
-    def put(self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None):
+    def put(
+        self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None
+    ):
         """Call api (PUT) and send response to callback."""
-        return self._call("PUT",
-                          endpoint=endpoint,
-                          data=data,
-                          json=json,
-                          callback=callback,
-                          callback_kwargs=callback_kwargs)
+        return self._call(
+            "PUT",
+            endpoint=endpoint,
+            data=data,
+            json=json,
+            callback=callback,
+            callback_kwargs=callback_kwargs,
+        )
 
-    def patch(self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None):
+    def patch(
+        self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None
+    ):
         """Call api (PATCH) and send response to callback."""
-        return self._call("PATCH",
-                          endpoint=endpoint,
-                          data=data,
-                          json=json,
-                          callback=callback,
-                          callback_kwargs=callback_kwargs)
+        return self._call(
+            "PATCH",
+            endpoint=endpoint,
+            data=data,
+            json=json,
+            callback=callback,
+            callback_kwargs=callback_kwargs,
+        )
 
-    def delete(self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None):
+    def delete(
+        self, endpoint=None, data=None, json=None, callback=None, callback_kwargs=None
+    ):
         """Call api (DELETE) and send response to callback."""
-        return self._call("DELETE",
-                          endpoint=endpoint,
-                          data=data,
-                          json=json,
-                          callback=callback,
-                          callback_kwargs=callback_kwargs)
+        return self._call(
+            "DELETE",
+            endpoint=endpoint,
+            data=data,
+            json=json,
+            callback=callback,
+            callback_kwargs=callback_kwargs,
+        )
